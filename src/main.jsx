b44d2224665c9a74884d2c56e7eda56c833827b2
khushpreet20 <khushisandhu0502@gmail.com>
2024-07-06 02:13:00 +0530

    Update BrowserRouter to HashRouter for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,30 @@ import './index.css'
 import Game from './components/Game.jsx'
 import Intro from './components/Intro.jsx'
 import Theme from './components/Theme.jsx'
-import { Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
-const router = createBrowserRouter([
+import { Router, RouterProvider, createBrowserRouter, HashRouter, createHashRouter } from 'react-router-dom'
+// const router = createBrowserRouter([
+//   {
+//     path: '/', 
+//     element: <App/>,
+//     children : [
+//       {
+//         path: '',
+//         element: <Intro/>
+//       },
+//       {
+//         path: 'game',
+//         element: <Game/>
+//       },
+//       {
+//         path:'theme',
+//         element: <Theme/>
+//       }
+//     ]
+//   }
+// ])
+// The above routing using Browser router doesn't works when deployed, so for this sole purpose of deploying the project of gh-pages, use HashRouter instead of BrowserRouter
+
+const router = createHashRouter([
   {
     path: '/', 
     element: <App/>,
@@ -26,6 +48,7 @@ const router = createBrowserRouter([
     ]
   }
 ])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
